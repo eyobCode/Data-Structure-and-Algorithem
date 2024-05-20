8901f8a8505b0e62aa50d39ee87c9e9e78c86d1c
eyob-mulugeta <eyobbmulugeta@gmail.com>
2024-05-20 17:28:31 +0300

    list node

--- LinkedList.java
+++ LinkedList.java
@@ -1,2 +1,14 @@
 public class LinkedList {
+    private ListNode head;
+
+    private static class ListNode{
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data=data;
+            this.next=null;
+        }
+
+    }
 }
