4d0d98073ca11c62852aa88cc901da61400f3604
eyob-mulugeta <eyobbmulugeta@gmail.com>
2024-05-23 19:18:20 +0300

    Generate Binary numbers from 1 to n using a Queue

---
+++ Queue/GenerateBinaryNumbers.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class GenerateBinaryNumbers {
+    public static String[] GenerateBinary(int number){
+        String[] result = new String[number];
+        Queue<String> temp = new LinkedList<>();
+
+        temp.offer("1");
+
+        for (int i = 0; i < number; i++) {
+            result[i] = temp.poll();
+
+            String n1 = result[i] + "0";
+            String n2 = result[i] + "1";
+
+            temp.offer(n1);
+            temp.offer(n2);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(GenerateBinary(3)));
+    }
+}
