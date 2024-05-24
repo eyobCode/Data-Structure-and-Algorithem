3a4ce63e59fab4fb99f36f952d5a3c5d54e3978c
eyob-mulugeta <eyobbmulugeta@gmail.com>
2024-05-24 17:45:03 +0300

    Merge Sort

--- Sort/mergeSort.java
+++ Sort/mergeSort.java
@@ -1,3 +1,10 @@
 public class mergeSort {
-
+    public static void Sort(int[] arr,int[] temp,int low,int high){
+        if(low < high){
+            int mid=low+(high - low)/2;
+            Sort(arr,temp,low,mid);
+            Sort(arr,temp,mid + 1,high);
+            merge(arr,temp,low,mid,high);
+        }
+    }
 }
