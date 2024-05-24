59e30ccd24ad7d0d8290b18e3c9cca24bdab7dc0
eyob-mulugeta <eyobbmulugeta@gmail.com>
2024-05-25 00:59:45 +0300

    Create Binary Tree

---
+++ BinaryTree/BinaryTree.java
@@ -0,0 +1,15 @@
+public class BinaryTree {
+
+    private TreeNode root;
+
+    private static class TreeNode{
+        private int data;
+        private TreeNode right;
+        private TreeNode left;
+
+        public TreeNode(int data){
+            this.data=data;
+        }
+    }
+
+}
