7695ef67b5f7e28f008012315a335fe70a841478
eyob-mulugeta <eyobbmulugeta@gmail.com>
2024-05-25 21:18:21 +0300

    Stack using Queues.

--- Stack/stackUsingOneQueue.java
+++ Stack/stackUsingOneQueue.java
@@ -1,3 +1,16 @@
 class stackUsingOneQueue {
 }
-class Queue{}
+class Queue{
+    private ListNode front;
+    private ListNode rear;
+    private int legth;
+
+    private class ListNode{
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data){
+
+        }
+    }
+}
