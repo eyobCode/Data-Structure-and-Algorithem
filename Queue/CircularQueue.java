c062107e3fcd6cc2ae7e34cfa69004ee80759a4d
eyob-mulugeta <eyobbmulugeta@gmail.com>
2024-05-25 14:24:58 +0300

    Circular Queue Linked list

--- Queue/CircularQueue.java
+++ Queue/CircularQueue.java
@@ -1,2 +1,22 @@
 public class CircularQueue {
+    private ListNode front;
+    private ListNode rear;
+    private int length;
+
+
+    private static class ListNode{
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    CircularQueue(){
+        front=null;
+        rear=null;
+        length=0;
+    }
+
 }
