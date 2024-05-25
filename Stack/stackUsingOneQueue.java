7c7d70974f8b302c551683435f5b43649fc96828
eyob-mulugeta <eyobbmulugeta@gmail.com>
2024-05-25 21:30:49 +0300

    Stack using Queues.

--- Stack/stackUsingOneQueue.java
+++ Stack/stackUsingOneQueue.java
@@ -10,7 +10,29 @@ class Queue{
         private ListNode next;
 
         public ListNode(int data){
-
+            this.data=data;
+            this.next=null;
         }
+    }
+    void Queue(){
+        front = null;
+        rear = null;
+        legth = 0;
+    }
+    public boolean isEmpty() {return legth == 0;}
+    public int size(){return legth;}
+    public int dequeue(){
+        if (isEmpty()){throw new RuntimeException("Queue is empty.")}
+        int temp = front.data;
+        front=front.next;
+        legth--;
+        return temp;
+    }
+    public void enqueue(int data){
+        ListNode node = new ListNode(data);
+        if (isEmpty()){front=node;}
+        else{rear.next=node;}
+        rear=node;
+        legth++;
     }
 }
