f145286f8749ccec509a6ac6cde15259e7f8ebbb
eyob <eyobbmulugeta@gmail.com>
2024-05-31 15:59:09 +0300

    add

--- Array- One Dimensional /AddingOrUpdatingArray.java
+++ Array- One Dimensional /AddingOrUpdatingArray.java
@@ -14,8 +14,21 @@ public class AddingOrUpdatingArray {
 
         // Declaration and Initialization
 
-        int[] array = new int[7];  //[0,0,0,0,0,0,0]
-        printArray(array);
+        int[] array = new int[4];
+        printArray(array); //[0,0,0,0,0,0,0]
+
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+
+        printArray(array); //1,2,3,4
+
+        System.out.println(array[array.length-1]); // last element 4
+
+        int[] arr2 = {4,3,2,1};
+        printArray(arr2);
+
 
     }
 
