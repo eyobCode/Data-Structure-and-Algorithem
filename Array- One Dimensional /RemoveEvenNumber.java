0d6c2fcbf6407f330fb1127d232e7c162d660435
eyobScript <eyobbmulugeta@gmaol.com>
2024-10-09 18:57:01 +0300

    Remove even number from array

--- Array- One Dimensional /RemoveEvenNumber.java
+++ Array- One Dimensional /RemoveEvenNumber.java
@@ -1,31 +1,36 @@
 import java.util.Arrays;
 
 public class RemoveEvenNumber {
-    public static int[] removeEven(int [] arr){
-        int oddNumber = 0; //count odd number
-
+    static int[] removeEven(int[] arr){
+        int oddCount = 0;
+        for(int i = 0; i < arr.length; i++){
+             if (arr[i] % 2 != 0){
+                 oddCount ++;
+             }
+        }
+        int[] result = new int[oddCount];
+        int idx = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 != 0){
-
-                oddNumber++;
-
+                result[idx] = arr[i];
+                idx++;
             }
-        }
-        int[] result =  new int[oddNumber];
-        int indx = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % 2 != 0 ){
-                result[indx] = arr[i];
-                indx++;
-            }
+
         }
         return result;
-
     }
+
+    public static void printer(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
     public static void main(String[] args) {
-        int[]  arr = {1,2,3,4,5,6,7};
+        int[] arrays = {2,3,4,5,6,1,7,9};
+        int[] newA = removeEven(arrays);
 
-        System.out.println(Arrays.toString(removeEven(arr)));
+        printer(newA);
+
     }
-
 }
